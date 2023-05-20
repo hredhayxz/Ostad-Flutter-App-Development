d2576827276455699eef60822e4a0a0743f9bbdd
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-05-20 14:01:04 +0600

    Live test solution
    Create a map (key value pair) of an employee, the keys will be employee = [Id, Name, Age, Salary, Gender].
    In this map [Id, Age, Salary] only these three keys' values will be in number.
    At last print out the Salary from the map.

---
+++ live test 01.dart
@@ -0,0 +1,17 @@
+/*
+Author: Md Alhaz Mondal Hredhay
+Student of Faridpur Polytechnic Institute
+GitHub & Linkedin: hredhayxz
+E-mail: amhredhay102@gmail.com
+*/
+
+void main() {
+  Map<String, dynamic> employeeInfo = {
+    "Id": 5001,
+    "Name": 'Md Alhaz Mondal Hredhay',
+    "Age": 21,
+    "Salary": 40000,
+    "Gender": 'Male'
+  };
+  print(employeeInfo["Salary"]);
+}
